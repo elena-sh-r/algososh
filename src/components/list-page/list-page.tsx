4577b8e0ce6ca4a9145ca3ae99d396af74e857b5
Elena Sharipova <elena.sh.r@yandex.ru>
2022-06-09 16:44:36 +0300

    feat: list algorithm in progress

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,10 +1,31 @@
 import React from "react";
+import styles from "./list-page.module.css";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+
 
 export const ListPage: React.FC = () => {
   return (
     <SolutionLayout title="Связный список">
-
+      <div className={`${styles.controlsContainer}`}>
+        <div className={`${styles.inputsContainer}`}>
+          <Input isLimitText={true} maxLength={4} placeholder="Введите значение" />
+          <Button text="Добавить в head" extraClass={`${styles.valueButton}`} />
+          <Button text="Добавить в tail" extraClass={`${styles.valueButton}`} />
+          <Button text="Удалить из head" extraClass={`${styles.valueButton}`} />
+          <Button text="Удалить из tail" extraClass={`${styles.valueButton}`} />
+        </div>
+        <div className={`${styles.inputsContainer}`}>
+          <Input placeholder="Введите индекс" />
+          <Button text="Добавить в head" extraClass={`${styles.indexButton}`} />
+          <Button text="Добавить в tail" extraClass={`${styles.indexButton}`} />
+        </div>
+      </div>
+      <div className={`${styles.circlesContainer}`}>
+        <Circle />
+      </div>
     </SolutionLayout>
   );
 };
