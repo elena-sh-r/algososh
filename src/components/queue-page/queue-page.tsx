98eccc861fbb2421ebc80922eef83e5130de1958
Elena Sharipova <elena.sh.r@yandex.ru>
2022-06-09 15:26:56 +0300

    feat: queue algorithm in progress

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import styles from "./queue-page.module.css";
+
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
 
 export const QueuePage: React.FC = () => {
   return (
     <SolutionLayout title="Очередь">
+      <div className={`${styles.controlsContainer}`}>
+        <div className={`${styles.inputsContainer}`}>
+          <Input isLimitText={true} maxLength={4} />
+          <Button text="Добавить" extraClass={`${styles.addButton}`} />
+          <Button text="Удалить" extraClass={`${styles.deleteButton}`} />
+        </div>
+        <Button text="Очистить" extraClass={`${styles.removeButton}`} />
+      </div>
+      <div className={`${styles.circlesContainer}`}>
+        <Circle />
+      </div>
 
     </SolutionLayout>
   );
