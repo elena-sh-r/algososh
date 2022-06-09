2944e94534d950629cbde6afc15f8b7a4bf87067
Elena Sharipova <elena.sh.r@yandex.ru>
2022-06-09 15:22:36 +0300

    feat: stack algorithm in progress

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import styles from "./stack-page.module.css";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
 
 export const StackPage: React.FC = () => {
   return (
     <SolutionLayout title="Стек">
-
+      <div className={`${styles.controlsContainer}`}>
+        <div className={`${styles.inputsContainer}`}>
+          <Input isLimitText={true} maxLength={4} />
+          <Button text="Добавить" extraClass={`${styles.addButton}`} />
+          <Button text="Удалить" extraClass={`${styles.deleteButton}`} />
+        </div>
+        <Button text="Очистить" extraClass={`${styles.removeButton}`} />
+      </div>
+      <div className={`${styles.circlesContainer}`}>
+        <Circle />
+      </div>
     </SolutionLayout>
   );
 };
