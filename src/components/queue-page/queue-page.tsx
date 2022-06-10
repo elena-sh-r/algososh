f9962d081e5d41f98efa4c85ec6156e97d4ab95f
Elena Sharipova <elena.sh.r@yandex.ru>
2022-06-10 17:34:44 +0300

    refactor: queue class implemented

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -2,75 +2,24 @@ import React, {useState, useEffect} from "react";
 import styles from "./queue-page.module.css";
 
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Queue } from "./queue";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
 
 export const QueuePage: React.FC = () => {
   const [value, setValue] = useState('');
-  const [headIdx, setHeadIdx] = useState(-1);
-  const [tailIdx, setTailIdx] = useState(-1);
   const [addedIdx, setAddedIdx] = useState(-1);
   const [deletedIdx, setDeletedIdx] = useState(-1);
-  const [elements, setElements] = useState<string[]>(['','','','','','',''])
+
+  const [render, setRender] = useState(false);
+  const [queue, setQueue] = useState(new Queue<string>(7, '', setRender, setValue, setAddedIdx, setDeletedIdx));
 
   const timeout = 200;
 
-  const enqueue = (item:string) => {
-    if (headIdx < 0) {
-      setHeadIdx(0);
-    }
-
-    let newTailIdx = tailIdx + 1;
-
-    if (newTailIdx > elements.length - 1) {
-      return;
-    }
-
-    const newElements = [...elements];
-    newElements[newTailIdx] = item;
-
-    setElements(newElements);
-    setTailIdx(newTailIdx);
-    setAddedIdx(newTailIdx);
-  }
-
-  const dequeue = () => {
-    let newHeadIdx = headIdx + 1;
-    const newElements = [...elements];
-    newElements[headIdx] = '';
-
-    setElements(newElements);
-
-    if (newHeadIdx > tailIdx) {
-      setHeadIdx(-1);
-      setTailIdx(-1);
-    } else {
-      setHeadIdx(newHeadIdx);
-    }
-
-    setDeletedIdx(-1);
-  }
-  
   const handleChange = (e: any) => {
     const value = e.target?.value;
     setValue(value);
-  }
-
-  const handleAdd = (e:any) => {
-    enqueue(value);
-    setValue('');
-  }
-  
-  const handleDelete = (e:any) => {
-    setDeletedIdx(headIdx);
-    setValue('');
-  }
-
-  const handleClear = (e:any) => {
-    setHeadIdx(-1);
-    setTailIdx(-1);
-    setElements(['','','','','','','']);
   }
 
   useEffect(() => {
@@ -86,24 +35,28 @@ export const QueuePage: React.FC = () => {
       return;
     }
 
-    setTimeout(()=>dequeue(), timeout);
+    setTimeout(()=>queue.dequeue(), timeout);
   }, [deletedIdx]);
+
+  useEffect(() => {
+    if (render){
+        setRender(false);
+    }
+  }, [render]);
 
   return (
     <SolutionLayout title="Очередь">
       <div className={`${styles.controlsContainer}`}>
         <div className={`${styles.inputsContainer}`}>
           <Input isLimitText={true} maxLength={4} value={value} onChange={handleChange} placeholder="Введите значение" />
-          <Button text="Добавить" onClick={handleAdd} extraClass={`${styles.addButton}`} disabled={!value || tailIdx === elements.length - 1} />
-          <Button text="Удалить" onClick={handleDelete} extraClass={`${styles.deleteButton}`} disabled={headIdx === -1 && tailIdx === -1} />
+          <Button text="Добавить" onClick={()=>queue.enqueue(value)} extraClass={`${styles.addButton}`} disabled={!value || queue.getTailIdx() === queue.getSize() - 1} />
+          <Button text="Удалить" onClick={()=>setDeletedIdx(queue.getHeadIdx())} extraClass={`${styles.deleteButton}`} disabled={queue.getHeadIdx() === -1 && queue.getTailIdx() === -1} />
         </div>
-        <Button text="Очистить" onClick={handleClear} extraClass={`${styles.removeButton}`} disabled={headIdx === -1 && tailIdx === -1} />
+        <Button text="Очистить" onClick={()=>queue.clear()} extraClass={`${styles.removeButton}`} disabled={queue.getHeadIdx() === -1 && queue.getTailIdx() === -1} />
       </div>
       <div className={`${styles.circlesContainer}`}>
-        {elements.map((letter:string, index:number) => 
-          <Circle key={index} extraClass={index === addedIdx || index === deletedIdx ? `${styles.changingCircleColor}` : ""} letter={letter} head={index === headIdx ? "head" : ""} tail={index.toString() + (index === tailIdx ?
-            `
-              tail` : "")} />
+        {queue.getElements().map((letter:string, index:number) => 
+          <Circle key={index} extraClass={index === addedIdx || index === deletedIdx ? `${styles.changingCircleColor}` : ""} letter={letter} head={index === queue.getHeadIdx() ? "head" : ""} index={index} tail={(index === queue.getTailIdx() ? `tail` : "")} />
         )}
       </div>
 
